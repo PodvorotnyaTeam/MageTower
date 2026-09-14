0934ea6e1d76b76db3087d0d3dd2971321b9f0dc
Masosos <YAdania2006@gmail.com>
2026-09-14 20:36:42 +0700

    ГОТОВО

--- Assets/Scripts/Cauldron/Ingridient.cs
+++ Assets/Scripts/Cauldron/Ingridient.cs
@@ -6,6 +6,25 @@ public class Ingridient : ScriptableObject
 {
     public string nameIngridient;
     public GameObject ingredientPrefab;
+
+    public int tocisity;
+    public int acidity;
+    public int volatility;
+    public int viscosity;
+    public int thermoactivity;
+    public int vitality;
+    public int conductivity;
+    public int psychoactivity;
+    public int entropy;
+
+    public List<int> attributes;
+
+    public string i = "ПИДАРАС";
     public float cookingTime;
     public float overcookingTime;
+
+    public void Awake()
+    {
+        attributes = new List<int> { tocisity, acidity, volatility, viscosity, thermoactivity, vitality, conductivity, psychoactivity, entropy };
+    }
 }
--- Assets/Scripts/Cauldron/Recipes.cs
+++ Assets/Scripts/Cauldron/Recipes.cs
@@ -5,6 +5,28 @@ using UnityEngine;
 public class Recipes : ScriptableObject
 {
     public string nameRecipe;
-    public List<Ingridient> ingridients;
+
+    public int tocisity;
+    public int acidity;
+    public int volatility;
+    public int viscosity;
+    public int thermoactivity;
+    public int vitality;
+    public int conductivity;
+    public int psychoactivity;
+    public int entropy;
+
+    public List<int> attributes;
+
+    public bool isCommon;
+    public bool isEpic;
+    public bool isIdeal;
+
+    public int maxDeviation;
+
     public GameObject result;
+    public void Awake()
+    {
+        attributes = new List<int> { tocisity, acidity, volatility, viscosity, thermoactivity, vitality, conductivity, psychoactivity, entropy };
+    }
 }
